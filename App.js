88d72903044d40dda0bc51b758c97ca11fb54410
gitmega17 <mega.1709@outlook.com>
2024-09-24 20:49:45 -0300

    2.0- add Screens

--- App.js
+++ App.js
@@ -1,60 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import GraphScreen from './screens/GraphScreen';
 
-// Registrando os componentes do Chart.js
-ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend);
+const Stack = createStackNavigator();
 
 export default function App() {
-  // Dados fictícios para o gráfico
-  const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-    datasets: [
-      {
-        label: 'Aquecimento',
-        data: [20, 45, 28, 80, 99, 43, 50],
-        borderColor: 'rgba(75, 192, 192, 1)',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        fill: false,
-      },
-    ],
-  };
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      tooltip: {
-        callbacks: {
-          label: function (tooltipItem) {
-            return `Aquecimento: ${tooltipItem.raw}`;
-          },
-        },
-      },
-    },
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Gráfico de Linhas</Text>
-      <Line data={data} options={options} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="GraphScreen" component={GraphScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
